d83948414f44d7cf97c1bcb3fa97409739ebe7d6
Oluwatosin Folaranmi <foluwatosin@ITILP_L014.simplexsystems.com>
2019-08-28 14:20:49 +0100

    Update Pie chart component

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,16 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number[] = [350, 450, 120];
+  pieChartLabels: string[] = ['A', 'B', 'C'];
+  colors: any[] = [
+    {
+      backgroundColor: ['#126fa2', '#a1722e', '#bb23a1']
+    }
+  ];
+  pieChartType = 'doughnut';
+
+
   ngOnInit() {
   }
 
